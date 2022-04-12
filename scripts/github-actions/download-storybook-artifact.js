6a4bee4624218c5f6b1828e9da2efa000ba162b9
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-04-12 11:53:31 +0100

    ci(GHA): Add final-ish versions of updated workflows

---
+++ scripts/github-actions/download-storybook-artifact.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+module.exports = async ({ github, context }) => {
+  const allArtifacts = await github.rest.actions.listWorkflowRunArtifacts({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.payload.workflow_run.id,
+  })
+
+  const matchArtifact = allArtifacts.data.artifacts.filter((artifact) => {
+    return artifact.name === 'storybook-static'
+  })[0]
+
+  const download = await github.rest.actions.downloadArtifact({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    artifact_id: matchArtifact.id,
+    archive_format: 'zip',
+  })
+
+  fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/storybook-static.zip`, Buffer.from(download.data))
+}
